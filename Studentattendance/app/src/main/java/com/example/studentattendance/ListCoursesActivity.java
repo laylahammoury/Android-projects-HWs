3e9b14f3a66da2d66277a23a368156235d7f3f06
yafa <yafa.farajallah.1998@gmail.com>
2019-04-14 22:01:50 +0300

    adding context menue

--- Studentattendance/app/src/main/java/com/example/studentattendance/ListCoursesActivity.java
+++ Studentattendance/app/src/main/java/com/example/studentattendance/ListCoursesActivity.java
@@ -2,6 +2,8 @@ package com.example.studentattendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -13,11 +15,13 @@ public class ListCoursesActivity extends AppCompatActivity {
     ListView coursesList;
     ArrayList<Courses> courses = new ArrayList<>();
     coursesAdapter coursesAdapter;
+    int course_pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_courses);
+        registerForContextMenu(coursesList);
 
         this.coursesList = findViewById(R.id.coursesList);
         this.databaseAdapter = new DatabaseAdapter(this);
@@ -29,6 +33,46 @@ public class ListCoursesActivity extends AppCompatActivity {
         this.coursesList.setAdapter(coursesAdapter);
         databaseAdapter.close();
 
+        coursesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 course_pos=position;
+
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+        AdapterView.AdapterContextMenuInfo info=  (AdapterView.AdapterContextMenuInfo)menuInfo;
+        if(v.getId()==R.id.coursesList)
+            getMenuInflater().inflate(R.menu.courses_menue,menu);
+
 
     }
+
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.item1:{
+
+
+                break;}
+            case R.id.item2:
+
+                break;
+            case R.id.item3:
+                courses.remove(course_pos);
+                break;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
 }
